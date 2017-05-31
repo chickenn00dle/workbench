15481f7598a9035b16520b556eb4914b47ec7b43
Rasmy Nguyen <rtnguyen87@gmail.com>
2017-05-31 22:29:35 +0900

    Adds gulp-concat dependency.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
+
+gulp.task('coffee', function(){
+    gulp.src('components/coffee/*.coffee')
+        .pipe(coffee({bare: true})
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function(){
+    gulp.src('components/scripts/*.js')
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+});
